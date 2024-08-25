ef76041a7b1c037b37e5891afa59bcf3d1654e48
Djari <abdelbassetd9@gmail.com>
2024-08-25 11:45:05 +0100

    intial commit - chapter 4

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Liu_Jian_Mao_Cao } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({weight : ["400" , '700'],subsets : ['latin']});
